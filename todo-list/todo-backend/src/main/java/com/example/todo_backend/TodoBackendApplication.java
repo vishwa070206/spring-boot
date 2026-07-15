342eb73576ab8a43027a7b2c24bb276f5d41ff6c
vishwa070206 <vishwanathang2006@gmail.com>
2026-07-15 22:32:10 +0530

    Receiving data

--- todo-list/todo-backend/src/main/java/com/example/todo_backend/TodoBackendApplication.java
+++ todo-list/todo-backend/src/main/java/com/example/todo_backend/TodoBackendApplication.java
@@ -1,6 +1,7 @@
 package com.example.todo_backend;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- todo-list/todo-backend/src/main/java/com/example/todo_backend/controller/HelloController.java
+++ todo-list/todo-backend/src/main/java/com/example/todo_backend/controller/HelloController.java
@@ -1,11 +1,25 @@
 package com.example.todo_backend.controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 public class HelloController{
-    @GetMapping("/hello")
-    public String hello(){
-        return "Hello Spring Boot!";
+//    @GetMapping("/sayHello")
+//    public String sayHello(@RequestParam String name){
+//        return "Hello "+name;
+//    }
+//
+//    @GetMapping("/tasks/{id}")
+//    public int getTask(@PathVariable int id){
+//        return id;
+//    }
+
+    @PostMapping("/tasks")
+    public String createTask(@RequestBody String task){
+        return task;
     }
+
+
+
+
 
 }
